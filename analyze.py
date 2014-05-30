91325bfee3f6e694812f48cbd078c01737dce0ef
Pekko Lipsanen <pekko.lipsanen@iki.fi>
2014-05-30 08:39:50 +0200

    Formating, smileys, shouters

--- analyze.py
+++ analyze.py
@@ -7,6 +7,8 @@ import re
 
 msgrow = re.compile("\[(?P<year>\d+)-(?P<month>\d+)-(?P<day>\d+) (?P<hour>\d+):(?P<minutes>\d+)\]  NappiGram (?P<name>.*) (»»»|>>>) (?P<msg>.*)")
 colors = re.compile("\x1b\[\d+(;\d+)?m")
+emoji = re.compile('[^\u0000-\uD7FF\uE000-\uFFFF]')
+
 
 def strip_colors(s):
     return colors.sub('', s)
@@ -34,17 +36,56 @@ def percentage(data, keyfunc, filterfunc):
     total = dict(count_group(data, keyfunc))
     filtered = count_group(data, keyfunc, filterfunc)
 
-    perc = [(key, filtered_count / total[key]) for (key, filtered_count) in filtered]
+    perc = [(key, filtered_count / total[key], filtered_count, total[key]) for (key, filtered_count) in filtered]
     return perc
+
+def _print_table_generic(title, data, formatfunc):
+    print()
+    print(title.upper())
+    print("="*len(title))
+    for r in data:
+        print(formatfunc(r))
+
+def print_table(title, data, limit=None):
+    if limit == None:
+        limit = len(data)
+    data = sorted(data, key=lambda x:x[1], reverse=True)
+    _print_table_generic(title, data[:limit], lambda x: "%d %s" % (x[1], x[0]))
+
+
+def print_table_perc(title, data, limit=None):
+    if limit == None:
+        limit = len(data)
+    data = sorted(data, key=lambda x:x[1], reverse=True)
+    _print_table_generic(title, data[:limit], lambda x: "%.2f%% (%d/%d)\t%s" % (x[1], x[2], x[3], x[0]))
 
 def main():
     def name(x): return x['name']
 
     msgs = get_msgs('output')
+
     count_by_name = count_group(msgs, name)
-    print(percentage(msgs, name, lambda x: ':)' in x['msg']))
+    print_table('Total', count_by_name, 20)
 
+    smileys = percentage(msgs, name, lambda x: ':)' in x['msg'])
+    print_table_perc('Happy people', smileys, 5)
 
+    def filter_curse(x):
+        cursewords = ['vittu', 'vitu', 'saatana', 'helvet', 'perkele', 'paska']
+        words = x['msg'].lower().split()
+        return any((w.startswith(c) for c in cursewords for w in words))
+
+    cursing = percentage(msgs, name, filter_curse)
+    print_table_perc('Cursing', cursing)
+
+    night = count_group(msgs, name, lambda x: 0 <= int(x['hour']) <= 5)
+    print_table('Night', night, 10)
+
+    shout = count_group(msgs, name, 
+        lambda x: emoji.sub('', x['msg'].upper()) == x['msg'] 
+            and len(x['msg']) > 3
+    )
+    print_table('Shouters', shout)
 
 
 
