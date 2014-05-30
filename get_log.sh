4b5827a0955d590e5c541abdc4864d04ed4fe774
Pekko Lipsanen <pekko.lipsanen@iki.fi>
2014-05-30 07:22:35 +0200

    get_log

--- get_log.sh
+++ get_log.sh
@@ -2,13 +2,21 @@
 
 DIR=$(cd $(dirname $0) && pwd)
 TG_DIR="tg"
-LIMIT="1000"
+LIMIT="5000"
+STEP="100"
 
 mkfifo tg_pipe
 cd "$TG_DIR"
 ./telegram -W < $DIR/tg_pipe > $DIR/output &
 cd "$DIR"
-echo "history NappiGram $LIMIT" > tg_pipe
-sleep 2
+
+for i in $(seq $LIMIT -$STEP 0); do
+	echo -n ".";
+	cmd="history NappiGram $STEP $i";
+	# echo $cmd;
+	echo $cmd > tg_pipe;
+	sleep 5;
+done
+
 echo "quit" > tg_pipe
 rm tg_pipe
